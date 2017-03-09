860d0d109037556b46ff75e807fca97293d99580
V2NileshSukalikar <nilesh.sukalikar@v2solutions.com>
2017-03-09 12:27:10 +0530

    menu changes
    menu

--- DynamicApp/src/app/app.component.ts
+++ DynamicApp/src/app/app.component.ts
@@ -12,13 +12,19 @@ export class AppComponent {
   title = 'app works!';
 
   data = {} as any;
+  menuFontContent: string;
   constructor(private globalservice: PagedataService, private router: Router) {
-
     this.data = globalservice.GlobalData;
   }
 
   getdata() {
     this.data = this.globalservice.GlobalData;
+  }
+
+  createMenuFontContent(menuName: string): string {
+    const matches = menuName.match(/\b(\w)/g);
+    const acronym = matches.join('');
+    return acronym;
   }
 
   setlink(url) {
